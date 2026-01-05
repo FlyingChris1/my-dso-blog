e2352725a64a29570238934d31c98e2cee89d18e
Christopher <christopherschuf2000@gmail.com>
2026-01-05 16:25:47 +0100

    My skills text bug fix

--- src/components/MySkillsComponent/MySkillsComponent.tsx
+++ src/components/MySkillsComponent/MySkillsComponent.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import styles from "./MySkillsComponent.module.css";
 
-
 export interface ISkillElement {
   label: string;
   imagePath: string;
-  imagePathRespo:string;
+  imagePathRespo: string;
   descriptions: string[];
 }
 
@@ -14,29 +13,15 @@ export interface ISkillsProps {
 }
 
 export default function MySkills({ skills = [] }: ISkillsProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const skillsPerPage = 3;
-  const totalPages = Math.ceil(skills.length / skillsPerPage);
-  const visibleSkills = skills.slice(
-    currentIndex,
-    currentIndex + skillsPerPage
-  );
-
-  const goToPage = (pageIndex: number) => {
-    setCurrentIndex(pageIndex * skillsPerPage);
-  };
-
   const [hoveredSkill, setHoveredSkill] =
     useState<ISkillElement | null>(null);
 
   return (
     <section id="skills" className={styles.mySkills}>
       <div className={styles.innerContent}>
-     
         <h2 className={styles.titleMedium}>My skills</h2>
-        <div className={styles.skillContent}>
-        
 
+        <div className={styles.skillContent}>
           {skills.map((skill) => (
             <div
               key={skill.label}
@@ -45,8 +30,8 @@ export default function MySkills({ skills = [] }: ISkillsProps) {
               onMouseLeave={() => setHoveredSkill(null)}
             >
               <img
-                src={skill.imagePath || ""}
-                alt={skill.label || ""}
+                src={skill.imagePath}
+                alt={skill.label}
                 className={styles.skillDetailImg}
               />
 
@@ -54,7 +39,9 @@ export default function MySkills({ skills = [] }: ISkillsProps) {
 
               {hoveredSkill === skill && (
                 <div className={styles.hoverDescriptionBox}>
-                  <h3 className={styles.titleHover}>How I used this skills</h3>
+                  <h3 className={styles.titleHover}>
+                    How I used this skill
+                  </h3>
                   <ul>
                     {skill.descriptions.map((desc, i) => (
                       <li key={i}>{desc}</li>
@@ -65,46 +52,6 @@ export default function MySkills({ skills = [] }: ISkillsProps) {
             </div>
           ))}
         </div>
-
-        <div className={styles.mobileSkillContent}>
-          <div className={styles.content}>
-            {visibleSkills.map((skill) => (
-              <div
-                key={skill.label}
-                className={styles.skillDetailMobile}
-              >
-                <img
-                    src={skill.imagePath}
-                    alt={skill.label}
-                    className={styles.skillDetailImg}
-                  />
-                <ul>
-                  {skill.descriptions.map((desc, i) => (
-                    <li key={i}>{desc}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-
- 
-        </div>
-
-                 <div className={styles.paginationDots}>
-            {Array.from({ length: totalPages }).map(
-              (_, pageIndex) => (
-                <div
-                  key={pageIndex}
-                  className={`${styles.dot} ${
-                    currentIndex / skillsPerPage === pageIndex
-                      ? styles.activeDot
-                      : ""
-                  }`}
-                  onClick={() => goToPage(pageIndex)}
-                />
-              )
-            )}
-          </div>
       </div>
     </section>
   );
